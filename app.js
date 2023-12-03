d3aa7de68a6ff1b2d73f14bd8293fc3ac98c13e7
Jonathan Fuentes <john.fuen95@gmail.com>
2023-12-02 21:08:11 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const one = document.querySelector('#one');
+const two = document.querySelector('#one');
+const three = document.querySelector('#one');
+const four = document.querySelector('#one');
+const five = document.querySelector('#one');
+const six = document.querySelector('#one');
+const seven = document.querySelector('#one');
+const eight = document.querySelector('#one');
+const nine = document.querySelector('#one');
+const plus = document.querySelector('#one');
+const minus = document.querySelector('#one');
+const divide = document.querySelector('#one');
+const mult = document.querySelector('#one');
+const percent = document.querySelector('#one');
+const clear = document.querySelector('#clear');
+const remove = document.querySelector('#one');
+const plusMinus = document.querySelector('#one');
+const equal = document.querySelector('#one');
+const period = document.querySelector('#one');
+const zero = document.querySelector('#one');
+const calc = document.querySelector('#calc');
+const input = document.querySelector('#input');
+let array = [];
+
+calc.addEventListener('click', (e) => {
+    if(e.target.className === 'buttons' && e.target.id !== 'clear'){
+     input.value = e.target.textContent;
+     if(typeof(+input.value) !== NaN){
+        array.push(+input.value)
+        console.log(array)
+     }
+     console.log(array)
+    }
+    
+})
+
+clear.addEventListener('click', () => {
+    input.value = '';
+    array = [];
+})
+
